486df71224d8a891f5d4ffd8e3c9fbbb605510bf
vishal patidar <vishalsuman8770@gmail.com>
2024-09-26 17:50:44 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,48 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import Home from '. /pages/Home'
+// import About from './pages/About'
+// import About from './pages/Signin'
+// import About from './pages/SignUp'
+// import About from './pages/Dashborad'
+// import About from './pages/Projects'
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />}/>
+//         <Route path="/about" element={<About />}/>
+//         <Route path=" /sign-in" element={<SignIn />} />
+//         <Route path=" /sign-up" element={<SignUp />} />
+//         <Route path="/ dashboard" element={<Dashboard />} />
+//         <Route path="/ Projects" element={<Projects />} />
+//       </Routes>
+    
+//     </BrowserRouter>
+//   )
+// }
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+
+import Dashboard from './pages/dashboard';
+
+import Projects from './pages/Projects';
+
 export default function App() {
   return (
-    <h1>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
